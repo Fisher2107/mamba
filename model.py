68b7d71ff6fb44c1094a3b5a367f3d061fad274e
Fisher2017 <umutcanhalil10@gmail.com>
2024-06-13 13:31:28 +0100

    using block from mamba

--- model.py
+++ model.py
@@ -1,53 +1,50 @@
 from mamba_ssm.modules.mamba_simple import Mamba
+from mamba_ssm.modules.block import Block
+from mamba_ssm.ops.triton.layer_norm import RMSNorm, layer_norm_fn, rms_norm_fn
 import torch
+from functools import partial
 import torch.nn as nn
 
 class MambaFull(nn.Module):
-    def __init__(self,args):
+    def __init__(self,d_model,city_count,nb_layers,coord_dim=2,mlp_cls=nn.Identity,norm_f=nn.LayerNorm):
         super().__init__()
-        self.d_model=args['d_model']
-        self.vocab_size=args['vocab_size']
+        self.d_model=d_model
+        self.city_count=city_count
+        self.nb_layers=nb_layers
+        self.mlp = mlp_cls(d_model)
+        self.norm_f = norm_f(d_model)
 
-        self.embedding = nn.Embedding(args['vocab_size'], args['d_model'])
-        self.layers = nn.ModuleList([ResidualBlock(args) for _ in range(args['n_layer'])])
-        self.final_norm = RMSNorm(args['d_model'])
-        self.output_head = nn.Linear(args['d_model'], args['vocab_size'], bias=False)
+        self.embedding = nn.Linear(coord_dim, d_model)
+        self.layers = nn.ModuleList([
+                    Block(dim= d_model,
+                        mixer_cls= partial(Mamba,d_model),
+                        mlp_cls= mlp_cls,
+                        fused_add_norm=True,
+                        residual_in_fp32=True,
+                        )   for _ in range(nb_layers)])
+        
+        self.output_head = nn.Linear(d_model,city_count, bias=False)
 
     def forward(self,x):
         x = self.embedding(x)
 
+        residual=None
         for layer in self.layers:
-            x = layer(x)
-            
-        x = self.final_norm(x)
+            x , residual = layer(x,residual)
+
+        x = layer_norm_fn(
+            x,
+            self.norm_f.weight,
+            self.norm_f.bias,
+            eps=self.norm_f.eps,
+            residual=residual,
+            prenorm=False,
+            residual_in_fp32=self.residual_in_fp32,
+            is_rms_norm=True
+        )
         logits = self.output_head(x)
+        #mask vistited cities
+        return x
 
-        return logits
-
-
-class ResidualBlock(nn.Module):
-    def __init__(self, args):
-        super().__init__()
-        self.args = args
-        self.mixer = Mamba(args['d_model'])
-        self.norm = RMSNorm(args['d_model'])
-        
-
-    def forward(self, x):
-        output = self.mixer(self.norm(x)) + x
-
-        return output
-
-class RMSNorm(nn.Module):
-    def __init__(self,
-                 d_model: int,
-                 eps: float = 1e-5):
-        super().__init__()
-        self.eps = eps
-        self.weight = nn.Parameter(torch.ones(d_model))
-
-
-    def forward(self, x):
-        output = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
-
-        return output
+def generate_data(device,batch_size,city_count,coord_dim=2):
+    return torch.rand(batch_size,city_count,2).long().to_device(device)
