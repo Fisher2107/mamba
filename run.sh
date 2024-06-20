2b2796de71ac80f4af0868554302c60ea600c381
Fisher2017 <umutcanhalil10@gmail.com>
2024-06-20 20:11:36 +0100

    experiment start token

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-python tsp.py --start = 5 --save_loc = 'checkpoints/start/Linear_mlp_5start'
-python tsp.py --start = 'rand' --save_loc = 'checkpoints/start/Linear_mlp_randstart'
+#!/bin/bash
+
+#python tsp.py --start 5 --save_loc 'checkpoints/start/Linear_mlp_5start'
+#python tsp.py --start 'rand' --save_loc 'checkpoints/start/Linear_mlp_randstart'
+python tsp.py --start 0 --save_loc 'checkpoints/start/Linear_mlp_0start'
+
