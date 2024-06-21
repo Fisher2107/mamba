c48255fdc9ba9bb3edc9be22550bcb563a32da9f
Fisher2017 <umutcanhalil10@gmail.com>
2024-06-21 14:37:10 +0100

    added big mlp exp

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 #python tsp.py --start 5 --save_loc 'checkpoints/start/Linear_mlp_5start'
 #python tsp.py --start 'rand' --save_loc 'checkpoints/start/Linear_mlp_randstart'
-python tsp.py --start 0 --save_loc 'checkpoints/start/Linear_mlp_0start'
+python tsp.py --start 2 --checkpoint 'checkpoints/start/Linear_mlp_2start_again_21-06_11-56.pt' --save_loc 'checkpoints/start/Linear_mlp_2start_again' --nb_epochs 2000
 
