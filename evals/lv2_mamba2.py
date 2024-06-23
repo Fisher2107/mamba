03bfb5cbac416d62e04aaf9a7beda412fc239179
Fisher2017 <umutcanhalil10@gmail.com>
2024-06-23 15:45:41 +0000

    mamba2 eval done

--- evals/lv2_mamba2.py
+++ evals/lv2_mamba2.py
@@ -22,37 +22,47 @@ class DotDict(dict):
         self.update(kwds)
         self.__dict__ = self
 
+checkpoint= torch.load('../checkpoints/mamba2/mamba_3lay_22-06_18-55.pt')
+checkpoint2 = torch.load('../checkpoints/mamba2/mamba_4lay_22-06_23-42.pt')
 
-#checkpoint= torch.load('../checkpoints/reverse/Linear_reverse4_21-06_17-02.pt')
+checkpoint3 = torch.load('../checkpoints/mamba2/mamba2_3lay_22-06_16-09.pt')
+checkpoint4 = torch.load('../checkpoints/mamba2/mamba2_4lay_22-06_17-16.pt')
 
-#args = checkpoint5['args']
-#print(args)
+print(checkpoint['time_tot'])
+print(checkpoint2['time_tot'])
+print(checkpoint3['time_tot'])
+print(checkpoint4['time_tot'])
 '''args.mlp_cls = 'identity'
 model_train = MambaFull(args.d_model, args.city_count, args.nb_layers, args.coord_dim, args.mlp_cls).to(device)
 model_train.load_state_dict(checkpoint['model_state_dict'])
 model_train.eval()'''
 
 
-#mean_tour_length_list = [tensor.cpu().numpy() for tensor in checkpoint['mean_tour_length_list']]
-
-#plt.plot(mean_tour_length_list, label='Reverse 4')
-
-
-#greedy = greedy_tsp(test_data,device=device)[0].item()
-#print(greedy)
-exact = exact_solver(test_data,device=device,split=1).item()
-print(exact)
+mean_tour_length_list = [tensor.cpu().numpy() for tensor in checkpoint['mean_tour_length_list']]
+mean_tour_length_list2 = [tensor.cpu().numpy() for tensor in checkpoint2['mean_tour_length_list']]
+mean_tour_length_list3 = [tensor.cpu().numpy() for tensor in checkpoint3['mean_tour_length_list']]
+mean_tour_length_list4 = [tensor.cpu().numpy() for tensor in checkpoint4['mean_tour_length_list']]
 
 
-'''plt.axhline(y=greedy, color='r', linestyle='--', label='Greedy Solver')
+
+plt.plot(mean_tour_length_list, label='m 3')
+plt.plot(mean_tour_length_list2, label='m 4')
+plt.plot(mean_tour_length_list3, label='m2 3')
+plt.plot(mean_tour_length_list4, label='m2 4')
+
+greedy = 3.1791656017303467
+exact = 2.8630127906799316
+
+
+plt.axhline(y=greedy, color='r', linestyle='--', label='Greedy Solver')
 plt.axhline(y=exact, color='g', linestyle='--', label='Exact Solver')
 
 # Add labels to the axes
 plt.xlabel('Epoch')
 plt.ylabel('Mean Tour Length')
-plt.ylim(2.1, 2.3)
+plt.ylim(2.7, 3.2)
 plt.title('All layers')
 
 plt.legend()
-plt.savefig('figs/reverse_all2.pdf')
-plt.show()'''
+plt.savefig('figs/mamba2_comparison.pdf')
+plt.show()
