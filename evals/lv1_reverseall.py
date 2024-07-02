06b2d5654ce79f50717809b84bb39176e779090e
Fisher2017 <umutcanhalil10@gmail.com>
2024-07-02 20:31:12 +0000

    added run reverse10

--- evals/lv1_reverseall.py
+++ evals/lv1_reverseall.py
@@ -30,6 +30,11 @@ checkpoint4 = torch.load('../checkpoints/reverse/Linear_3_21-06_16-30.pt')
 
 checkpoint5 = torch.load('../checkpoints/reverse/Linear_reverse5_21-06_17-44.pt')
 checkpoint6 = torch.load('../checkpoints/reverse/Linear_5_21-06_18-35.pt')
+
+checkpoint7 = torch.load('../checkpoints/reverse/mamba2_reversestart_3l_01-07_10-43.pt')
+checkpoint8 = torch.load('../checkpoints/reverse/mamba2_reversestart_4l_01-07_11-20.pt')
+checkpoint9 = torch.load('../checkpoints/reverse/mamba2_reversestart_5l_01-07_12-03.pt')
+
 args = checkpoint5['args']
 print(args)
 '''args.mlp_cls = 'identity'
@@ -44,6 +49,9 @@ mean_tour_length_list3 = [tensor.cpu().numpy() for tensor in checkpoint3['mean_t
 mean_tour_length_list4 = [tensor.cpu().numpy() for tensor in checkpoint4['mean_tour_length_list']]
 mean_tour_length_list5 = [tensor.cpu().numpy() for tensor in checkpoint5['mean_tour_length_list']]
 mean_tour_length_list6 = [tensor.cpu().numpy() for tensor in checkpoint6['mean_tour_length_list']]
+mean_tour_length_list7 = [tensor.cpu().numpy() for tensor in checkpoint7['mean_tour_length_list']]
+mean_tour_length_list8 = [tensor.cpu().numpy() for tensor in checkpoint8['mean_tour_length_list']]
+mean_tour_length_list9 = [tensor.cpu().numpy() for tensor in checkpoint9['mean_tour_length_list']]
 
 
 plt.plot(mean_tour_length_list3, label='Reverse 3')
@@ -52,9 +60,12 @@ plt.plot(mean_tour_length_list5, label='Reverse 5')
 plt.plot(mean_tour_length_list4, label='Standard 3')
 plt.plot(mean_tour_length_list2, label='Standard 4')
 plt.plot(mean_tour_length_list6, label='Standard 5')
+plt.plot(mean_tour_length_list7, label='RS 3')
+plt.plot(mean_tour_length_list8, label='RS 4')
+plt.plot(mean_tour_length_list9, label='RS 5')
 
 greedy = greedy_tsp(test_data)[0].item()
-exact = exact_solver(test_data).item()
+exact = exact_solver(test_data,device='cuda').item()
 print(greedy)
 print(exact)
 
@@ -69,5 +80,5 @@ plt.ylim(2.1, 2.3)
 plt.title('All layers')
 
 plt.legend()
-plt.savefig('figs/reverse_all2.pdf')
+plt.savefig('figs/reverse_all3.pdf')
 plt.show()
