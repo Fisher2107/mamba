5d74b6c0856de55c160f4bc03a71c52db7a04a36
Fisher2017 <umutcanhalil10@gmail.com>
2024-07-03 21:58:30 +0100

    almost finish runs

---
+++ jobmaker.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# 1st arg - runtime in hours of each job
+
+# Counter for job number
+job_count=1
+
+# Read run.sh line by line
+while IFS= read -r line
+do
+    # Check if the line starts with "python"
+    if [[ $line == python* ]]
+    then
+        # Create a new job script for each Python command
+        job_file="jobs/job_${job_count}.sh"
+        
+        # Write the job script
+        cat << EOF > "$job_file"
+#!/bin/bash
+#$ -N job_${job_count}
+#$ -wd /exports/eddie/scratch/s2517783/mamba
+#$ -l h_rt=${1}:00:00
+#$ -q gpu
+#$ -pe gpu-a100 1
+#$ -l h_vmem=80G
+#$ -l rl9=true
+
+. /etc/profile.d/modules.sh
+. /exports/applications/support/set_qlogin_environment.sh
+
+module load cuda/12.1.1
+
+source /exports/eddie/scratch/s2517783/miniconda3/bin/activate base
+cd /exports/eddie/scratch/s2517783/mamba
+conda activate tspp
+
+$line
+EOF
+
+        # Make the job script executable
+        chmod +x "$job_file"
+        # Increment job count
+        ((job_count++))
+    fi
+done < run.sh
+
+echo "Created and submitted ${job_count} jobs."
