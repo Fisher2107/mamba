d679a0b65b07a8aaa92ee8f47b0426c098d3af92
Fisher2017 <umutcanhalil10@gmail.com>
2024-07-06 21:10:15 +0000

    fourier evals mostly done

--- evals/lvl1_embed.py
+++ evals/lvl1_embed.py
@@ -17,37 +17,59 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 test_data_loc=f'../data/start_2/{test_size}_{city_count}_{coord_dim}.pt'
 test_data = torch.load(test_data_loc).to(device)
 
+plt.style.use('bmh')
+
 class DotDict(dict):
     def __init__(self, **kwds):
         self.update(kwds)
         self.__dict__ = self
 
-checkpoint = torch.load('../checkpoints/try_same_3/Linear1_18-06_12-34.pt')
-checkpoint2 = torch.load('../checkpoints/embed/fourier2_19-06_13-46.pt')
+#5city 3layer
+'''checkpoint = torch.load('../checkpoints/embed2/fourier1_5city_3l_06-07_06-17.pt')
+checkpoint2 = torch.load('../checkpoints/embed2/fourier2_5city_3l_05-07_22-04.pt')
+checkpoint3 = torch.load('../checkpoints/embed2/fourier5_5city_3l_05-07_17-09.pt')
+checkpoint4 = torch.load('../checkpoints/embed2/fourier10_5city_3l_05-07_23-04.pt')
+checkpoint5 = torch.load('../checkpoints/embed2/linear_5city_3l_05-07_23-11.pt')'''
 
-checkpoint3 = torch.load('../checkpoints/embed/Linear_mlp_4lay_20-06_13-11.pt')
-checkpoint4 = torch.load('../checkpoints/embed/fourier2_mlp_19-06_12-11.pt')
+#5city 4layer
+'''checkpoint = torch.load('../checkpoints/embed2/fourier1_5city_4l_05-07_21-38.pt')
+checkpoint2 = torch.load('../checkpoints/embed2/fourier2_5city_4l_05-07_20-37.pt')
+checkpoint3 = torch.load('../checkpoints/embed2/fourier5_5city_4l_06-07_07-24.pt')
+checkpoint4 = torch.load('../checkpoints/embed2/fourier10_5city_4l_05-07_20-11.pt')
+checkpoint5 = torch.load('../checkpoints/embed2/linear_5city_4l_06-07_00-11.pt')'''
 
-checkpointlist = [checkpoint, checkpoint2, checkpoint3, checkpoint4]
-print([i['args'] for i in checkpointlist])
-'''args.mlp_cls = 'identity'
-model_train = MambaFull(args.d_model, args.city_count, args.nb_layers, args.coord_dim, args.mlp_cls).to(device)
-model_train.load_state_dict(checkpoint['model_state_dict'])
-model_train.eval()'''
+#10city 3layer
+'''checkpoint = torch.load('../checkpoints/embed2/fourier1_10city_3l_06-07_09-10.pt')
+checkpoint2 = torch.load('../checkpoints/embed2/fourier2_10city_3l_06-07_01-37.pt')
+checkpoint3 = torch.load('../checkpoints/embed2/fourier5_10city_3l_06-07_08-51.pt')
+checkpoint4 = torch.load('../checkpoints/embed2/fourier10_10city_3l_06-07_03-54.pt')
+checkpoint5 = torch.load('../checkpoints/embed2/linear_10city_3l_05-07_18-18.pt')'''
 
+#10city 4layer
+checkpoint = torch.load('../checkpoints/embed2/fourier1_10city_4l_06-07_00-17.pt')
+checkpoint2 = torch.load('../checkpoints/embed2/fourier2_10city_4l_06-07_03-17.pt')
+checkpoint3 = torch.load('../checkpoints/embed2/fourier10_10city_4l_06-07_06-11.pt')
+checkpoint4 = torch.load('../checkpoints/embed2/fourier10_10city_4l_06-07_06-11.pt')
+checkpoint5 = torch.load('../checkpoints/embed2/linear_10city_4l_05-07_17-11.pt')
 
 mean_tour_length_list = [tensor.cpu().numpy() for tensor in checkpoint['mean_tour_length_list']]
 mean_tour_length_list2 = [tensor.cpu().numpy() for tensor in checkpoint2['mean_tour_length_list']]
 mean_tour_length_list3 = [tensor.cpu().numpy() for tensor in checkpoint3['mean_tour_length_list']]
 mean_tour_length_list4 = [tensor.cpu().numpy() for tensor in checkpoint4['mean_tour_length_list']]
+mean_tour_length_list5 = [tensor.cpu().numpy() for tensor in checkpoint5['mean_tour_length_list']]
 
-plt.plot(mean_tour_length_list3, label='Linear Embedding')
-plt.plot(mean_tour_length_list4, label='Fourier Embedding')
+plt.plot(mean_tour_length_list, label='Fourier1 ')
+plt.plot(mean_tour_length_list2, label='Fourier2 ')
+plt.plot(mean_tour_length_list3, label='Fourier5 ')
+plt.plot(mean_tour_length_list4, label='Fourier10 ')
+plt.plot(mean_tour_length_list5, label='Linear ')
 #plt.plot(mean_tour_length_list3)
 #plt.plot(mean_tour_length_list4)
 
-greedy = greedy_tsp(test_data)[0].item()
-exact = exact_solver(test_data).item()
+#greedy = greedy_tsp(test_data)[0].item()
+#exact = exact_solver(test_data,device='cuda').item()
+greedy = 3.1791656017303467
+exact = 2.8630127906799316
 print(greedy)
 print(exact)
 
@@ -58,8 +80,8 @@ plt.axhline(y=exact, color='g', linestyle='--', label='Exact Solver')
 # Add labels to the axes
 plt.xlabel('Epoch')
 plt.ylabel('Mean Tour Length')
-plt.ylim(2.1, 2.64)
+#plt.ylim(2.1, 2.64)
 
 plt.legend()
-#plt.savefig('figs/mean_tour_length_embed2.pdf')
+plt.savefig('figs/10_city/embed_4l.pdf')
 plt.show()
