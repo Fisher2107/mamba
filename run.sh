59cbd674353b08a8f2c54d51b26c99d5ee3603c8
Fisher2017 <umutcanhalil10@gmail.com>
2024-07-16 22:24:08 +0100

    added ng runs

--- run.sh
+++ run.sh
@@ -13,11 +13,9 @@
 #python tsp.py --save_loc 'checkpoints/mamba2/mamba1' --nb_layers 3  --nb_epochs 10 --city_count 10 --recycle_data 10 --mamba2 True
 #python tsp.py --save_loc 'checkpoints/mamba2/mamba1' --nb_layers 3  --nb_epochs 5 --city_count 10 --recycle_data 10 --nb_batch_per_epoch 20 --mamba2 True
 
-#Pointer layers
-#python tsp.py --save_loc 'checkpoints/pointer_layers/layer1' --nb_layers 1  --nb_epochs 2000 --city_count 10 --last_layer 'pointer' 
-#python tsp.py --save_loc 'checkpoints/pointer_layers/layer2' --nb_layers 2  --nb_epochs 2000 --city_count 10 --last_layer 'pointer'
-#python tsp.py --save_loc 'checkpoints/pointer_layers/layer3' --nb_layers 3  --nb_epochs 2000 --city_count 10 --last_layer 'pointer'
-#python tsp.py --save_loc 'checkpoints/pointer_layers/layer4' --nb_layers 4  --nb_epochs 2000 --city_count 10 --last_layer 'pointer'
+#Pointer layers-ng
+python tsp.py --save_loc 'checkpoints/mamba_ng/city10/lay3_bsz150_bpe10' --nb_layers 3  --nb_epochs 2000 --mamba2 True --city_count 10 --last_layer 'pointer' --reverse True --mlp_cls 'identity'
+python tsp.py --save_loc 'checkpoints/mamba_ng/city50/lay3_bsz150_bpe10' --nb_layers 3  --nb_epochs 100  --mamba2 True --city_count 50 --nb_batch_per_epoch 40 --bsz 150 --last_layer 'pointer' --reverse True --mlp_cls 'identity'
 
 #Pointer Generalisation
 #python tsp.py --save_loc 'checkpoints/pointer_generalisation/normal_pointer' --nb_layers 3  --nb_epochs 2000 --city_count 10 --last_layer 'pointer' #Test to see if anything changed
@@ -32,6 +30,6 @@
 #python tsp.py --save_loc 'checkpoints/city50/mamba2_profiler_150' --nb_layers 3  --nb_epochs 5 --mamba2 True --city_count 50 --nb_batch_per_epoch 1 --bsz 150 --last_layer 'pointer' --reverse True --profiler True
 #python tsp.py --save_loc 'checkpoints/city50/mamba2_profiler_100' --nb_layers 3  --nb_epochs 5 --mamba2 True --city_count 50 --nb_batch_per_epoch 1 --bsz 100 --last_layer 'pointer' --reverse True --profiler True
 
-python tsp.py --save_loc 'checkpoints/gpu/mamba2_10_recy5' --nb_layers 3  --nb_epochs 5 --city_count 10 --recycle_data 5 --mamba2 True --pynvml True --gpu_id 1 --wandb
+#python tsp.py --save_loc 'checkpoints/gpu/mamba2_10_recy5' --nb_layers 3  --nb_epochs 5 --city_count 10 --recycle_data 5 --mamba2 True --pynvml True --gpu_id 1 --wandb
 
 ####
