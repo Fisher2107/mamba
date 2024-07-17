e20673e38c8880256b6874f299a5a9a92ec34589
Fisher2017 <umutcanhalil10@gmail.com>
2024-07-17 10:56:43 +0100

    organise

--- evals/gpu/plot_maker.sh
+++ evals/gpu/plot_maker.sh
@@ -1,29 +1,66 @@
 #!/bin/bash
 
+#NO_POINT
+##################
 #bsz
-#python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz10_gpu_stats_15-07_16-13' --events 'bsz/mamba2_50_bsz10_events_15-07_16-13'  --output_file 'bsz/mamab2_50_bsz10.pdf' --x_lim_start 0 --x_lim_end 0
-#python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz20_gpu_stats_15-07_16-22' --events 'bsz/mamba2_50_bsz20_events_15-07_16-22'  --output_file 'bsz/mamab2_50_bsz20.pdf' --x_lim_start 0 --x_lim_end 0
-python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz100_gpu_stats_15-07_16-26' --events 'bsz/mamba2_50_bsz100_events_15-07_16-26'  --output_file 'bsz/mamab2_50_bsz100.pdf' --x_lim_start 0 --x_lim_end 0
-python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz150_gpu_stats_15-07_16-28' --events 'bsz/mamba2_50_bsz150_events_15-07_16-28'  --output_file 'bsz/mamab2_50_bsz150.pdf' --x_lim_start 0 --x_lim_end 0
-python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz200_gpu_stats_15-07_16-30' --events 'bsz/mamba2_50_bsz200_events_15-07_16-30'  --output_file 'bsz/mamab2_50_bsz200.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz10_gpu_stats_15-07_16-13' --events 'bsz/mamba2_50_bsz10_events_15-07_16-13'  --output_file 'no_point/bsz/mamab2_50_bsz10.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz20_gpu_stats_15-07_16-22' --events 'bsz/mamba2_50_bsz20_events_15-07_16-22'  --output_file 'no_point/bsz/mamab2_50_bsz20.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz100_gpu_stats_15-07_16-26' --events 'bsz/mamba2_50_bsz100_events_15-07_16-26'  --output_file 'no_point/bsz/mamab2_50_bsz100.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz150_gpu_stats_15-07_16-28' --events 'bsz/mamba2_50_bsz150_events_15-07_16-28'  --output_file 'no_point/bsz/mamab2_50_bsz150.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'bsz/mamba2_50_bsz200_gpu_stats_15-07_16-30' --events 'bsz/mamba2_50_bsz200_events_15-07_16-30'  --output_file 'no_point/bsz/mamab2_50_bsz200.pdf' --x_lim_start 0 --x_lim_end 0
 
 #city_count
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_5_d16_gpu_stats_15-07_15-58' --events 'city_count/mamba2_5_d16_events_15-07_15-58'  --output_file 'city_count/mamba2_5_d16.pdf' --x_lim_start 0 --x_lim_end 0
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_5_gpu_stats_15-07_15-45' --events 'city_count/mamba2_5_events_15-07_15-45'  --output_file 'city_count/mamba2_5.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_5_d16_gpu_stats_15-07_15-58' --events 'city_count/mamba2_5_d16_events_15-07_15-58'  --output_file 'no_point/city_count/mamba2_5_d16.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_5_gpu_stats_15-07_15-45' --events 'city_count/mamba2_5_events_15-07_15-45'  --output_file 'no_point/city_count/mamba2_5.pdf' --x_lim_start 0 --x_lim_end 0
 
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_10_d16_gpu_stats_15-07_16-01' --events 'city_count/mamba2_10_d16_events_15-07_16-01'  --output_file 'city_count/mamba2_10_d16.pdf' --x_lim_start 0 --x_lim_end 0
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_10_gpu_stats_15-07_15-46' --events 'city_count/mamba2_10_events_15-07_15-46'  --output_file 'city_count/mamba2_10.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_10_d16_gpu_stats_15-07_16-01' --events 'city_count/mamba2_10_d16_events_15-07_16-01'  --output_file 'no_point/city_count/mamba2_10_d16.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_10_gpu_stats_15-07_15-46' --events 'city_count/mamba2_10_events_15-07_15-46'  --output_file 'no_point/city_count/mamba2_10.pdf' --x_lim_start 0 --x_lim_end 0
 
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_20_d16_gpu_stats_15-07_16-02' --events 'city_count/mamba2_20_d16_events_15-07_16-02'  --output_file 'city_count/mamba2_20_d16.pdf' --x_lim_start 0 --x_lim_end 0
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_20_gpu_stats_15-07_15-48' --events 'city_count/mamba2_20_events_15-07_15-48'  --output_file 'city_count/mamba2_20.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_20_d16_gpu_stats_15-07_16-02' --events 'city_count/mamba2_20_d16_events_15-07_16-02'  --output_file 'no_point/city_count/mamba2_20_d16.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_20_gpu_stats_15-07_15-48' --events 'city_count/mamba2_20_events_15-07_15-48'  --output_file 'no_point/city_count/mamba2_20.pdf' --x_lim_start 0 --x_lim_end 0
 
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_50_d16_gpu_stats_15-07_16-04' --events 'city_count/mamba2_50_d16_events_15-07_16-04'  --output_file 'city_count/mamba2_50_d16.pdf' --x_lim_start 0 --x_lim_end 0
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_50_gpu_stats_15-07_15-49' --events 'city_count/mamba2_50_events_15-07_15-49'  --output_file 'city_count/mamba2_50.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_50_d16_gpu_stats_15-07_16-04' --events 'city_count/mamba2_50_d16_events_15-07_16-04'  --output_file 'no_point/city_count/mamba2_50_d16.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_50_gpu_stats_15-07_15-49' --events 'city_count/mamba2_50_events_15-07_15-49'  --output_file 'no_point/city_count/mamba2_50.pdf' --x_lim_start 0 --x_lim_end 0
 
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_100_d16_gpu_stats_15-07_16-07' --events 'city_count/mamba2_100_d16_events_15-07_16-07'  --output_file 'city_count/mamba2_100_d16.pdf' --x_lim_start 0 --x_lim_end 0
-#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_100_gpu_stats_15-07_15-53' --events 'city_count/mamba2_100_events_15-07_15-53'  --output_file 'city_count/mamba2_100.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_100_d16_gpu_stats_15-07_16-07' --events 'city_count/mamba2_100_d16_events_15-07_16-07'  --output_file 'no_point/city_count/mamba2_100_d16.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'city_count/mamba2_100_gpu_stats_15-07_15-53' --events 'city_count/mamba2_100_events_15-07_15-53'  --output_file 'no_point/city_count/mamba2_100.pdf' --x_lim_start 0 --x_lim_end 0
 
 #lay
-#python vis_gpu_args.py --gpu_stats 'lay/mamba2_50_lay1_gpu_stats_15-07_15-40' --events 'lay/mamba2_50_lay1_events_15-07_15-40'  --output_file 'lay/mamba2_lay1.pdf' --x_lim_start 0 --x_lim_end 0
-#python vis_gpu_args.py --gpu_stats 'lay/mamba2_50_lay2_gpu_stats_15-07_15-41' --events 'lay/mamba2_50_lay2_events_15-07_15-41'  --output_file 'lay/mamba2_lay2.pdf' --x_lim_start 0 --x_lim_end 0
-#python vis_gpu_args.py --gpu_stats 'lay/mamba2_50_lay3_gpu_stats_15-07_15-43' --events 'lay/mamba2_50_lay3_events_15-07_15-43'  --output_file 'lay/mamba2_lay3.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'lay/mamba2_50_lay1_gpu_stats_15-07_15-40' --events 'lay/mamba2_50_lay1_events_15-07_15-40'  --output_file 'no_point/lay/mamba2_lay1.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'lay/mamba2_50_lay2_gpu_stats_15-07_15-41' --events 'lay/mamba2_50_lay2_events_15-07_15-41'  --output_file 'no_point/lay/mamba2_lay2.pdf' --x_lim_start 0 --x_lim_end 0
+#python vis_gpu_args.py --gpu_stats 'lay/mamba2_50_lay3_gpu_stats_15-07_15-43' --events 'lay/mamba2_50_lay3_events_15-07_15-43'  --output_file 'no_point/lay/mamba2_lay3.pdf' --x_lim_start 0 --x_lim_end 0
+#############
+
+#POINT
+##################
+#bsz
+python vis_gpu_args.py --gpu_stats 'bsz_point/mamba2_50_bsz10_gpu_stats_17-07_04-17' --events 'bsz_point/mamba2_50_bsz10_events_17-07_04-17'  --output_file 'point/bsz/mamab2_50_bsz10.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'bsz_point/mamba2_50_bsz100_gpu_stats_17-07_04-30' --events 'bsz_point/mamba2_50_bsz100_events_17-07_04-30'  --output_file 'point/bsz/mamab2_50_bsz100.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'bsz_point/mamba2_50_bsz200_gpu_stats_17-07_04-34' --events 'bsz_point/mamba2_50_bsz200_events_17-07_04-34'  --output_file 'point/bsz/mamab2_50_bsz200.pdf' --x_lim_start 0 --x_lim_end 0
+
+#city_count
+python vis_gpu_args.py --gpu_stats 'city_count_point/mamba2_100_d16_gpu_stats_15-07_16-07' --events 'city_count_point/mamba2_100_d16_events_15-07_16-07'  --output_file 'point/city_count/mamba2_100_d16.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'city_count_point/mamba2_100_gpu_stats_15-07_15-53' --events 'city_count_point/mamba2_100_events_15-07_15-53'  --output_file 'point/city_count/mamba2_100.pdf' --x_lim_start 0 --x_lim_end 0
+
+#lay
+python vis_gpu_args.py --gpu_stats 'lay_point/mamba2_50_lay1_gpu_stats_15-07_15-40' --events 'lay_point/mamba2_50_lay1_events_15-07_15-40'  --output_file 'point/lay/mamba2_lay1.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'lay_point/mamba2_50_lay2_gpu_stats_15-07_15-41' --events 'lay_point/mamba2_50_lay2_events_15-07_15-41'  --output_file 'point/lay/mamba2_lay2.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'lay_point/mamba2_50_lay3_gpu_stats_15-07_15-43' --events 'lay_point/mamba2_50_lay3_events_15-07_15-43'  --output_file 'point/lay/mamba2_lay3.pdf' --x_lim_start 0 --x_lim_end 0
+#############
+
+#NG
+##################
+#bsz
+python vis_gpu_args.py --gpu_stats 'bsz_point_ng/mamba2_50_bsz10_gpu_stats_15-07_16-13' --events 'bsz_point_ng/mamba2_50_bsz10_events_15-07_16-13'  --output_file 'point_ng/bsz/mamab2_50_bsz10.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'bsz_point_ng/mamba2_50_bsz100_gpu_stats_15-07_16-26' --events 'bsz_point_ng/mamba2_50_bsz100_events_15-07_16-26'  --output_file 'point_ng/bsz/mamab2_50_bsz100.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'bsz_point_ng/mamba2_50_bsz200_gpu_stats_15-07_16-30' --events 'bsz_point_ng/mamba2_50_bsz200_events_15-07_16-30'  --output_file 'point_ng/bsz/mamab2_50_bsz200.pdf' --x_lim_start 0 --x_lim_end 0
+
+#city_count
+python vis_gpu_args.py --gpu_stats 'city_count_point_ng/mamba2_100_d16_gpu_stats_15-07_16-07' --events 'city_count_point_ng/mamba2_100_d16_events_15-07_16-07'  --output_file 'point_ng/city_count/mamba2_100_d16.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'city_count_point_ng/mamba2_100_gpu_stats_15-07_15-53' --events 'city_count_point_ng/mamba2_100_events_15-07_15-53'  --output_file 'point_ng/city_count/mamba2_100.pdf' --x_lim_start 0 --x_lim_end 0
+
+#lay
+python vis_gpu_args.py --gpu_stats 'lay_point_ng/mamba2_50_lay1_gpu_stats_15-07_15-40' --events 'lay_point_ng/mamba2_50_lay1_events_15-07_15-40'  --output_file 'point_ng/lay/mamba2_lay1.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'lay_point_ng/mamba2_50_lay2_gpu_stats_15-07_15-41' --events 'lay_point_ng/mamba2_50_lay2_events_15-07_15-41'  --output_file 'point_ng/lay/mamba2_lay2.pdf' --x_lim_start 0 --x_lim_end 0
+python vis_gpu_args.py --gpu_stats 'lay_point_ng/mamba2_50_lay3_gpu_stats_15-07_15-43' --events 'lay_point_ng/mamba2_50_lay3_events_15-07_15-43'  --output_file 'point_ng/lay/mamba2_lay3.pdf' --x_lim_start 0 --x_lim_end 0
+#############
