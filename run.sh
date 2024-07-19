3d723cd5e3bce1d087fb291850d375cf9c1d58a0
Fisher2107 <umutcanhalil10@gmail.com>
2024-07-19 19:10:20 +0000

    tspp plot made

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #NG 64
-python tspp.py --save_loc 'checkpoints/tspp/city10' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 10 --nb_batch_per_epoch 10 --bsz 100 --last_layer 'pointer' --reverse True --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
-python tspp.py --save_loc 'checkpoints/tspp/city20' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 20 --nb_batch_per_epoch 10 --bsz 100 --last_layer 'pointer' --reverse True --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
-python tspp.py --save_loc 'checkpoints/tspp/city50' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 50 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
-python tspp.py --save_loc 'checkpoints/tspp/city75' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 75 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
-python tspp.py --save_loc 'checkpoints/tspp/city100' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 100 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
-python tspp.py --save_loc 'checkpoints/tspp/city125' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 125 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
-python tspp.py --save_loc 'checkpoints/tspp/city150' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 150 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
-python tspp.py --save_loc 'checkpoints/tspp/city175' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 175 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
-python tspp.py --save_loc 'checkpoints/tspp/city200' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 200 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id $CUDA_VISIBLE_DEVICES 
+python tspp.py --save_loc 'checkpoints/tspp/city125' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 125 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city150' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 150 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city175' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 175 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city250' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 250 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city300' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 300 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city400' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 400 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city500' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 500 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city600' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 600 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city700' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 700 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
+python tspp.py --save_loc 'checkpoints/tspp/city800' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 800 --nb_batch_per_epoch 40 --bsz 100 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 
 
 #Pointer Generalisation
 #python tsp.py --save_loc 'checkpoints/pointer_generalisation/normal_pointer' --nb_layers 3  --nb_epochs 2000 --city_count 10 --last_layer 'pointer' #Test to see if anything changed
