ab31a6fed320951734b2651afc15ca08f9ef1d81
Fisher2107 <umutcanhalil10@gmail.com>
2024-07-20 12:08:04 +0000

    action next city tried

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-#action = tour
-python tsp.py --save_loc 'checkpoints/tspp/city10' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 10 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --project_name 'action'
-python tsp.py --save_loc 'checkpoints/tspp/city20' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 20 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --project_name 'action'
-python tsp.py --save_loc 'checkpoints/tspp/city50' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 50 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --project_name 'action' 
-python tsp.py --save_loc 'checkpoints/tspp/city100' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 100 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 --project_name 'action'
-
 #action = next_city
-python tsp.py --save_loc 'checkpoints/tspp/city10_next_city' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 10 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --project_name 'action' --action 'next_city'
-python tsp.py --save_loc 'checkpoints/tspp/city20_next_city' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 20 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --project_name 'action' --action 'next_city'
-python tsp.py --save_loc 'checkpoints/tspp/city50_next_city' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 50 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --project_name 'action' --action 'next_city'
-python tsp.py --save_loc 'checkpoints/tspp/city100_next_city' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 100 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 --project_name 'action' --action 'next_city'
+python tsp.py --save_loc 'checkpoints/action/city10_next_city' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 10 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --wandb --action 'next_city'
+python tsp.py --save_loc 'checkpoints/action/city20_next_city' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 20 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --wandb --action 'next_city'
+python tsp.py --save_loc 'checkpoints/action/city50_next_city' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 50 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --wandb --action 'next_city'
+python tsp.py --save_loc 'checkpoints/action/city100_next_city' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 100 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 --wandb --action 'next_city'
+
+#action = tour
+python tsp.py --save_loc 'checkpoints/action/city10' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 10 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --wandb
+python tsp.py --save_loc 'checkpoints/action/city20' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 20 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --wandb
+python tsp.py --save_loc 'checkpoints/action/city50' --nb_layers 3  --nb_epochs 10  --mamba2 True --city_count 50 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True --pynvml True --gpu_id 0 --wandb 
+python tsp.py --save_loc 'checkpoints/action/city100' --nb_layers 3  --nb_epochs 10 --mamba2 True --city_count 100 --nb_batch_per_epoch 10 --bsz 60 --last_layer 'pointer' --reverse True  --pynvml True --gpu_id 0 --wandb
+
+
 
 #Pointer Generalisation
 #python tsp.py --save_loc 'checkpoints/pointer_generalisation/normal_pointer' --nb_layers 3  --nb_epochs 2000 --city_count 10 --last_layer 'pointer' #Test to see if anything changed
