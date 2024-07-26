3e4594d30b990897e4a8c5dc9fc3c9ccc84c103b
Fisher2017 <umutcanhalil10@gmail.com>
2024-07-26 14:14:24 +0100

    created action_v_tour.py

--- evals/gpu/scale_plot_maker_citycount.py
+++ evals/gpu/scale_plot_maker_citycount.py
@@ -11,7 +11,7 @@ def generate_plot(csv_files,csv_files16,csv_files_lay1,csv_files_ng,saveto):
     
     for csv_file in csv_files:
         df = pd.read_csv(csv_file)
-        memory_usage = df['Memory Usage'].iloc[-1]
+        memory_usage = df['Memory Usage'].iloc[-1] 
         y_values.append(memory_usage)
         
     for csv_file in csv_files16:
@@ -30,17 +30,17 @@ def generate_plot(csv_files,csv_files16,csv_files_lay1,csv_files_ng,saveto):
         y_values_ng.append(memory_usage)
 
     #least squares fit
-    z64 = np.polyfit(x_values, y_values, 2)
+    z64 = np.polyfit(x_values, y_values, 1)
     x_range = np.arange(0, 150, 0.1)
     plt.plot(x_range, np.polyval(z64, x_range), label='quad fit dim = 64',alpha=0.5)
 
-    z16 = np.polyfit(x_values, y_values_16, 2)
+    z16 = np.polyfit(x_values, y_values_16, 1)
     plt.plot(x_range, np.polyval(z16, x_range), label='quad fit dim = 16',alpha=0.5)
 
-    zlay1 = np.polyfit(x_values, y_values_lay1, 2)
+    zlay1 = np.polyfit(x_values, y_values_lay1, 1)
     plt.plot(x_range, np.polyval(zlay1, x_range), label='quad fit lay1',alpha=0.5)
 
-    zng = np.polyfit(x_values, y_values_ng, 2)
+    zng = np.polyfit(x_values, y_values_ng, 1)
     plt.plot(x_range, np.polyval(zng, x_range), label='quad fit ng',alpha=0.5)
 
 
@@ -56,47 +56,48 @@ def generate_plot(csv_files,csv_files16,csv_files_lay1,csv_files_ng,saveto):
     plt.plot(x_values, y_values_ng, marker='o', linestyle='None', label='ng',ms=5)
     plt.xlabel('City Count')
     plt.ylabel('Memory Usage (GB)')
+    plt.ylim(0, 10)
     plt.title('Memory Usage required to train model by City Count')
     plt.legend()
     plt.savefig(saveto)
     #plt.show()
 
-csv_files= ['../../checkpoints/gpu/tour/mamba2_5_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_10_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_20_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_50_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_75_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_100_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_120_gpu_stats.csv',]
+csv_files= ['../../checkpoints/gpu/action2/mamba2_5_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_10_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_20_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_50_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_75_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_100_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_120_gpu_stats.csv',]
 
-csv_files_16 = ['../../checkpoints/gpu/tour/mamba2_5_d16_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_10_d16_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_20_d16_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_50_d16_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_75_d16_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_100_d16_gpu_stats.csv',
-                '../../checkpoints/gpu/tour/mamba2_120_d16_gpu_stats.csv']
+csv_files_16 = ['../../checkpoints/gpu/action2/mamba2_5_d16_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_10_d16_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_20_d16_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_50_d16_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_75_d16_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_100_d16_gpu_stats.csv',
+                '../../checkpoints/gpu/action2/mamba2_120_d16_gpu_stats.csv']
 
-csv_files_lay1 = ['../../checkpoints/gpu/tour/mamba2_5_lay1_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_10_lay1_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_20_lay1_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_50_lay1_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_75_lay1_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_100_lay1_gpu_stats.csv',
-                '../../checkpoints/gpu/tour/mamba2_120_lay1_gpu_stats.csv']
+csv_files_lay1 = ['../../checkpoints/gpu/action2/mamba2_5_lay1_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_10_lay1_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_20_lay1_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_50_lay1_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_75_lay1_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_100_lay1_gpu_stats.csv',
+                '../../checkpoints/gpu/action2/mamba2_120_lay1_gpu_stats.csv']
 
-csv_files_ng = ['../../checkpoints/gpu/tour/mamba2_5_ng_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_10_ng_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_20_ng_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_50_ng_gpu_stats.csv', 
-              '../../checkpoints/gpu/tour/mamba2_75_ng_gpu_stats.csv',
-              '../../checkpoints/gpu/tour/mamba2_100_ng_gpu_stats.csv',
-                '../../checkpoints/gpu/tour/mamba2_120_ng_gpu_stats.csv']
-
-
+csv_files_ng = ['../../checkpoints/gpu/action2/mamba2_5_ng_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_10_ng_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_20_ng_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_50_ng_gpu_stats.csv', 
+              '../../checkpoints/gpu/action2/mamba2_75_ng_gpu_stats.csv',
+              '../../checkpoints/gpu/action2/mamba2_100_ng_gpu_stats.csv',
+                '../../checkpoints/gpu/action2/mamba2_120_ng_gpu_stats.csv']
 
 
 
 
 
-generate_plot(csv_files,csv_files_16,csv_files_lay1,csv_files_ng,'../figs/scale/memory_usage_tour.pdf')
+
+
+generate_plot(csv_files,csv_files_16,csv_files_lay1,csv_files_ng,'../figs/scale/memory_usage_action.pdf')
