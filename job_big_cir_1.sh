80f2e887945bb9b0b896834d8c470bc477d7d60c
user name <s2517783@cirrus-login3.ib0.icexa.epcc.ed.ac.uk>
2024-08-02 08:30:49 +0100

    ran cirrus big

--- job_big_cir_1.sh
+++ job_big_cir_1.sh
@@ -10,8 +10,17 @@
 
 # Load the required modules
 module load nvidia/nvhpc/24.5
+
 pwd
 source ../miniconda3/bin/activate
 conda activate base
 
-python tsp.py --save_loc 'checkpoints/big_cirrus/64_G_city20' --nb_layers 3  --nb_epochs 6000  --mamba2 True --city_count 20 --nb_batch_per_epoch 10 --bsz 600 --last_layer 'pointer' --reverse True --action 'next_city'  --project_name 'big_cirrus' 
+# Set the TRANSFORMERS_CACHE environment variable to a writable directory
+export TRANSFORMERS_CACHE="${PWD}/.cache/huggingface/hub"
+mkdir -p "${TRANSFORMERS_CACHE}"
+
+# Set the Triton cache directory to a writable location
+export TRITON_CACHE_DIR="${PWD}/triton_cache"
+mkdir -p "${TRITON_CACHE_DIR}"
+
+python tsp.py --save_loc 'checkpoints/big_cirrus/64_G_city20' --nb_layers 3  --nb_epochs 6000  --mamba2 True --city_count 20 --nb_batch_per_epoch 10 --bsz 600 --last_layer 'pointer' --reverse True --action 'next_city'  --project_name 'big_cirrus' --wandb
