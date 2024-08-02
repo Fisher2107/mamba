107d59b1e4cbb2d3011217130e877abab94b9025
Fisher2107 <umutcanhalil10@gmail.com>
2024-08-02 12:45:40 +0000

    fix

--- jobs/job_final_6.sh
+++ jobs/job_final_6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#$ -N job_final_2
+#$ -N job_final_6
 #$ -wd /exports/eddie/scratch/s2517783/mamba
-#$ -l h_rt=24:00:00
+#$ -l h_rt=40:00:00
 #$ -q gpu
 #$ -pe gpu-a100 1
 #$ -l h_vmem=80G
@@ -16,4 +16,4 @@ source /exports/eddie/scratch/s2517783/miniconda3/bin/activate base
 cd /exports/eddie/scratch/s2517783/mamba
 conda activate tsp
 
-python tsp.py --save_loc 'checkpoints/big/BIG_city20_2'  --nb_epochs 12000  --mamba2 True --city_count 20 --nb_batch_per_epoch 10  --bsz 600 --reverse True  --project_name 'Mamba_big' --checkpoint 'checkpoints/big_cirrus/64_G_city20.pt'
+python tsp.py --save_loc 'checkpoints/abalation/city20_3_128'  --nb_epochs 10000  --mamba2 True --city_count 20 --nb_batch_per_epoch 10  --bsz 600 --reverse False  --project_name 'Abalation' --d_model 128
