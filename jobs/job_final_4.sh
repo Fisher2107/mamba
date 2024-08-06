34f8553673e84b652a38da53f95922cbc8b9aac1
Fisher2017 <umutcanhalil10@gmail.com>
2024-08-06 19:29:33 +0100

    added final eval runs from cirrus

--- jobs/job_final_4.sh
+++ jobs/job_final_4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -N job_final_4
 #$ -wd /exports/eddie/scratch/s2517783/mamba
-#$ -l h_rt=30:00:00
+#$ -l h_rt=24:00:00
 #$ -q gpu
 #$ -pe gpu-a100 1
 #$ -l h_vmem=80G
@@ -16,4 +16,4 @@ source /exports/eddie/scratch/s2517783/miniconda3/bin/activate base
 cd /exports/eddie/scratch/s2517783/mamba
 conda activate tsp
 
-python tsp.py --save_loc 'checkpoints/abalation/city20_256'  --nb_epochs 10000  --mamba2 True --city_count 20 --nb_batch_per_epoch 10  --bsz 600 --reverse True  --project_name 'Abalation' --d_model 256 --nb_layers 1 
+python tsp.py --save_loc 'checkpoints/abalation/city20_128_R'  --nb_epochs 10000  --mamba2 True --city_count 20 --nb_batch_per_epoch 10  --bsz 600 --reverse_start True  --project_name 'Abalation' --d_model 128 --nb_layers 1 
