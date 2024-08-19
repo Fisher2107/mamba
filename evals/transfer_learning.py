c480e14e4905283e55ca2a42b163ba58ad6c7804
Fisher2017 <umutcanhalil10@gmail.com>
2024-08-19 18:09:06 +0000

    transfer figs complete

--- evals/transfer_learning.py
+++ evals/transfer_learning.py
@@ -37,29 +37,37 @@ mean_tour_length_list4 = [tensor.cpu().numpy() for tensor in checkpoint4['mean_t
 
 mean_tour_length_lists = [mean_tour_length_list, mean_tour_length_list2, mean_tour_length_list3, mean_tour_length_list4,]
 
-#plt.plot(mean_tour_length_list, label='No Transfer')
-#plt.plot(mean_tour_length_list2[4000:], label='Transferred weights from city count 10')
-plt.plot(mean_tour_length_list3, label='No Transfer')
-plt.plot(mean_tour_length_list4[4000:], label='Transferred weights from city count 20')
+plt.plot(mean_tour_length_list, label='No Transfer')
+plt.plot(mean_tour_length_list2[4000:], label='Transferred weights from city count 10')
+#plt.plot(mean_tour_length_list3, label='No Transfer')
+#plt.plot(mean_tour_length_list4[4000:], label='Transferred weights from city count 20')
 
 
+# Increase font sizes
+plt.rcParams['font.size'] = 14
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['axes.titlesize'] = 18
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 12
 
-#greedy = 4.4843
-greedy = 6.9641
-#exact = 3.85
-exact = 5.70
+greedy = 4.4843
+#greedy = 6.9641
+exact = 3.85
+#exact = 5.70
 
 print(greedy)
 print(exact)
-plt.ylim(5.68,7)
+plt.ylim(3.8,4.5)
+#plt.ylim(5.68,7)
 
 plt.axhline(y=greedy, color='r', linestyle='--', label='Greedy Solver')
 plt.axhline(y=exact, color='g', linestyle='--', label='Exact Solver')
 
 # Add labels to the axes
-plt.xlabel('Epoch')
-plt.ylabel('Mean Tour Length')
+plt.xlabel('Epoch', fontsize=16)
+plt.ylabel('Mean Tour Length', fontsize=16)
 plt.title('Transfer Learning vs No Transfer Learning')
 plt.legend()
-plt.savefig('figs/scale/transfer/50_transfer.pdf')
+plt.savefig('figs/scale/transfer/20_transfer.pdf')
 #plt.show()
