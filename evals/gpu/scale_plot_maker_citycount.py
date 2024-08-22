aa502020455ac589dc570b3bac2d4f632d4cb9a9
Fisher2017 <umutcanhalil10@gmail.com>
2024-08-22 16:40:11 +0100

    created new scaling figs

--- evals/gpu/scale_plot_maker_citycount.py
+++ evals/gpu/scale_plot_maker_citycount.py
@@ -2,62 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 plt.style.use('bmh')
-def generate_plot_memory(csv_files,saveto):
-    x_values = [5, 10, 20, 40, 60, 80, 100]
-    total_memory = []
-    memroy_train = []
-    memroy_eval = []
-    
-    for csv_file in csv_files:
-        df = pd.read_csv(csv_file)
-        memory_usage = df['Memory Usage'].iloc[-1] 
-        total_memory.append(memory_usage)
-
-    for csv_file in csv_files:
-        df_events = pd.read_csv(csv_file.replace('gpu_stats','events'), parse_dates=[0])
-        df = pd.read_csv(csv_file, parse_dates=[0])
-
-        # Find the timestamp for 'Epoch 0, Step 19'
-        timestamp = df_events[df_events['Event'].str.contains('Epoch 0, Step 15')].iloc[0, 0]
-
-        # Find the first row in df where the timestamp is later than the extracted timestamp
-        row = df[df['Timestamp'] > timestamp].iloc[0]
-
-        # Return the value from the third column
-        value = row.iloc[2]
-        memroy_train.append(value)
-    
-    memroy_eval = [total_memory[i] - memroy_train[i] for i in range(len(total_memory))]
-
-        
-
-    #least squares fit
-    total_mem = np.polyfit(x_values, total_memory, 2)
-    x_range = np.arange(0, 100, 0.1)
-    plt.plot(x_range, np.polyval(total_mem, x_range), label='total')
-
-    train_mem = np.polyfit(x_values, memroy_train, 2)
-    x_range = np.arange(0, 100, 0.1)
-    plt.plot(x_range, np.polyval(train_mem, x_range), label='train')
-
-    train_evl = np.polyfit(x_values, memroy_eval, 2)
-    x_range = np.arange(0, 100, 0.1)
-    plt.plot(x_range, np.polyval(train_evl, x_range), label='eval')
-
-
-
-    #plot
-    plt.plot(x_values, total_memory, marker='o', linestyle='None',ms=5)
-    plt.plot(x_values, memroy_train, marker='o', linestyle='None',ms=5)
-    plt.plot(x_values, memroy_eval, marker='o', linestyle='None',ms=5)
-
-    plt.xlabel('City Count')
-    plt.ylabel('Memory Usage (GB)')
-    plt.title('Memory Usage required to train model by City Count')
-    plt.legend()
-    plt.savefig(saveto)
-    plt.close()
-    #plt.show()
 
 def generate_plot_memory_train(csv_files,saveto):
     x_values = [5, 10, 20, 40, 60, 80, 100]
@@ -92,14 +36,12 @@ def generate_plot_memory_train(csv_files,saveto):
     x_range = np.arange(0, 100, 0.1)
     plt.plot(x_range, np.polyval(train_mem, x_range), label='memory scaling')
 
-
-
     plt.plot(x_values, memroy_train, marker='o', linestyle='None',ms=5)
     #plt.plot(x_values, total_memory, marker='o', linestyle='None',ms=5, label='total')
 
-
-    plt.xlabel('City Count')
-    plt.ylabel('Memory Usage (GB)')
+    plt.rcParams['legend.fontsize'] = 14
+    plt.xlabel('City Count',fontsize=16)
+    plt.ylabel('Memory Usage (GB)',fontsize=16)
     plt.title('Memory Usage required to train model by City Count')
     plt.ylim(0, 22)
     plt.legend()
@@ -153,8 +95,9 @@ def generate_plot_time(csv_files,saveto):
     plt.plot(x_values, time_train, marker='o', linestyle='None',ms=5)
     plt.plot(x_values, time_eval, marker='o', linestyle='None',ms=5)
     
-    plt.xlabel('City Count')
-    plt.ylabel('Time Taken (s)')
+    plt.rcParams['legend.fontsize'] = 14
+    plt.xlabel('City Count',fontsize=16)
+    plt.ylabel('Time Taken (s)',fontsize=16)
     plt.title('Time Taken required to train model by City Count')
     plt.legend()
     if 'importance_sampling' in saveto:
@@ -164,14 +107,6 @@ def generate_plot_time(csv_files,saveto):
             
     plt.savefig(saveto)
     plt.close()
-
-csv_files_action= ['../../checkpoints/gpu/action_lightning/mamba2_5_gpu_stats.csv',
-              '../../checkpoints/gpu/action_lightning/mamba2_10_gpu_stats.csv', 
-              '../../checkpoints/gpu/action_lightning/mamba2_20_gpu_stats.csv', 
-              '../../checkpoints/gpu/action_lightning/mamba2_40_gpu_stats.csv', 
-              '../../checkpoints/gpu/action_lightning/mamba2_60_gpu_stats.csv',
-              '../../checkpoints/gpu/action_lightning/mamba2_80_gpu_stats.csv',
-              '../../checkpoints/gpu/action_lightning/mamba2_100_gpu_stats.csv',]
 
 csv_files_tour= ['../../checkpoints/gpu/tour_lightning/mamba2_5_gpu_stats.csv',
               '../../checkpoints/gpu/tour_lightning/mamba2_10_gpu_stats.csv', 
@@ -197,19 +132,10 @@ csv_file_importance_sampling= ['../../checkpoints/gpu/importance_sampling_lightn
                 '../../checkpoints/gpu/importance_sampling_lightning/standard_mamba2_80_gpu_stats.csv',
                 '../../checkpoints/gpu/importance_sampling_lightning/standard_mamba2_100_gpu_stats.csv',]
 
+generate_plot_memory_train(csv_files_tour,'../figs/scale/lightning/memory_usage_train_tour2.pdf')
+generate_plot_memory_train(csv_file_importance_sampling5,'../figs/scale/lightning/memory_usage_train_importance_sampling52.pdf')
+generate_plot_memory_train(csv_file_importance_sampling,'../figs/scale/lightning/memory_usage_train_importance_sampling_standard2.pdf')
 
-generate_plot_memory(csv_files_action,'../figs/scale/lightning/memory_usage_action.pdf')
-generate_plot_memory(csv_files_tour,'../figs/scale/lightning/memory_usage_tour.pdf')
-generate_plot_memory(csv_file_importance_sampling5,'../figs/scale/lightning/memory_usage_importance_sampling5.pdf')
-generate_plot_memory(csv_file_importance_sampling,'../figs/scale/lightning/memory_usage_importance_sampling_standard.pdf')
-
-
-generate_plot_memory_train(csv_files_action,'../figs/scale/lightning/memory_usage_train_action.pdf')
-generate_plot_memory_train(csv_files_tour,'../figs/scale/lightning/memory_usage_train_tour.pdf')
-generate_plot_memory_train(csv_file_importance_sampling5,'../figs/scale/lightning/memory_usage_train_importance_sampling5.pdf')
-generate_plot_memory_train(csv_file_importance_sampling,'../figs/scale/lightning/memory_usage_train_importance_sampling_standard.pdf')
-'''
-generate_plot_time(csv_files_action,'../figs/scale/lightning/time_usage_action.pdf')
-generate_plot_time(csv_files_tour,'../figs/scale/lightning/time_usage_tour.pdf')
-generate_plot_time(csv_file_importance_sampling5,'../figs/scale/lightning/time_usage_importance_sampling5.pdf')
-generate_plot_time(csv_file_importance_sampling,'../figs/scale/lightning/time_usage_importance_sampling_standard.pdf')'''
+generate_plot_time(csv_files_tour,'../figs/scale/lightning/time_usage_tour2.pdf')
+generate_plot_time(csv_file_importance_sampling5,'../figs/scale/lightning/time_usage_importance_sampling52.pdf')
+generate_plot_time(csv_file_importance_sampling,'../figs/scale/lightning/time_usage_importance_sampling_standard2.pdf')
